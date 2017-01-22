111c1f89b61dace6e73fb94c105bdf1f9561ceaa
etheleon <picy2k@gmail.com>
2017-01-22 11:50:19 +0800

    Wesley - First commit

---
+++ wesley/README.md
@@ -0,0 +1,6 @@
+# Lung Image Preprocessing
+
+## Introduction
+
+This is my first adventure into Deep learning and Image processing at the same
+time. Wish me luck
---
+++ wesley/script/prepro.0100.dicom2image.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import dicom
+import os
+import scipy.ndimage
+import matplotlib.pyplot as plt
+
+from skimage import measure, morphology
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: int(x.InstanceNumber))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+    for s in slices:
+        s.SliceThickness = slice_thickness
+    return slices
+
+def get_pixels_hu(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    # Convert to int16 (from sometimes int16),
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+    # Convert to Hounsfield units (HU)
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+    image += np.int16(intercept)
+    return np.array(image, dtype=np.int16)
+
+
+
+first_patient = load_scan(INPUT_FOLDER + patients[0])
+first_patient_pixels = get_pixels_hu(first_patient)
+plt.hist(first_patient_pixels.flatten(), bins=80, color='c')
+plt.xlabel("Hounsfield Units (HU)")
+plt.ylabel("Frequency")
+plt.show()
+
+# Show some slice in the middle
+plt.imshow(first_patient_pixels[80], cmap=plt.cm.gray)
+plt.show()
